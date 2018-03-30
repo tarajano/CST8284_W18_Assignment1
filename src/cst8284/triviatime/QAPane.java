6411dc82094ef994005d11d50037a44a5000b786
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2018-03-30 19:47:32 -0400

    Ads radio buttons (to Answers) in AQPane.getAnswerPane()
    TODO: Figue out how to add control buttons ("That's My answer").

--- src/cst8284/triviatime/QAPane.java
+++ src/cst8284/triviatime/QAPane.java
@@ -7,24 +7,74 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 public class QAPane {
    private RadioButton[] rbAr;
    private VBox qaPane;
-    
-   // TODO: Add a QAPane constructor that takes as an argument QA object.  The object's
+   
+   // TODO: Add a QAPane constructor that takes as an argument QA object. The object's
    // should be used to load the question and potential answer into the center pane
-   // along with radio buttons next to each answer.  Rather than use the default font,
+   // along with radio buttons next to each answer.
+   // TODO Rather than use the default font,
    // your output will look considerably better is you use the .setStyle() method
    // to improve the look of the user interface.
    // See the Assignment 1 document for details on implementing the button handler
    // for this method
+   public QAPane(QA qa) {
+     
+//     VBox questionVBox = this.getSingleStringVBox(qa.getQuestion());
+//     VBox answersVBox = this.getAnswerPane(qa.getAnswers());
+//     VBox explanationVBox = this.getSingleStringVBox(qa.getExplanation());
+     
+     //qaPane.getChildren().addAll(questionVBox,answersVBox,explanationVBox);
+     //qaPane.getChildren().addAll(answersVBox);
+   }
 
    // TODO: write a method getAnswerPane() that takes as arguments an array of Strings
    // corresponding to the array of answers returned by getAnswers() and returns
    // a VBox loaded with radio buttons next to each string.  You'll need to load
    // each radio button into a toggle group.  Again, see the Assignment 1 document
    // for details
+   public VBox getAnswerPane(String[] answers) { // maybe this method must be private
+     ToggleGroup radioBtnGroup = new ToggleGroup();
+     VBox vbox = new VBox();
+     VBox btnvbox = new VBox();
+     btnvbox.setSpacing(5);
+     RadioButton rb;
+     
+     for (String answer : answers) {
+       rb = new RadioButton(answer);
+       rb.setToggleGroup(radioBtnGroup);
+       btnvbox.getChildren().add(rb);
+     }
+     vbox.getChildren().add(btnvbox);
+     return vbox;
+   }
+   
+   public VBox getQuestionPane(String question) { // maybe this method must be private
+     VBox vbox = new VBox();
+     vbox.getChildren().add( this.setTxt(question) );
+     return vbox;
+   }
+   
+   public VBox getSingleStringVBox(String string) { // maybe this method must be private
+     VBox vbox = new VBox();
+     vbox.getChildren().add( this.setTxt(string) );
+     return vbox;
+   }
+   
+   private Text setTxt(String text) {
+     Text t = new Text();
+//   t.setFill(Color.BLUE);
+//   t.setFont(Font.font(null, FontWeight.BOLD, 32));
+     t.setText(text);
+     return t;
+   }
+   
    
    // TODO write a method getRadioButtonSelected() that returns the number of the 
    // radio button selected.  One way to do this is to loop through each radio button
