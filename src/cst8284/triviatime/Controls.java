ca980b29f4dad1ff768ade3a91fc7fac58567bb0
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2018-04-01 23:18:52 -0400

    Finally centered the centerPane from Controls class.
    TODO: Fix the centering and padding issue for Results class.
    
    FILE:    src/cst8284/triviatime/Controls.java
      ADDS: setNxtPane() plus BorderPane.setMargin(nxtPane, new Insets(10,10,10,200)) to center content in the center Pane.

--- src/cst8284/triviatime/Controls.java
+++ src/cst8284/triviatime/Controls.java
@@ -3,6 +3,7 @@ package cst8284.triviatime;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -160,7 +161,7 @@ public class Controls {
   private static void setNxtPane() {
     VBox nxtPane = getNxtQPane(); 
     bp.setCenter(nxtPane);
-    BorderPane.setAlignment(nxtPane, Pos.CENTER );
+    BorderPane.setMargin(nxtPane, new Insets(10, 10, 10, 200) );
   }
   
   
